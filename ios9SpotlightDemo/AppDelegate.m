dc2b8ab03c86d565c3efd0be300bfe896b6309df
Rocky <542622608@qq.com>
2015-09-28 12:06:51 +0800

    finish

--- ios9SpotlightDemo/AppDelegate.m
+++ ios9SpotlightDemo/AppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import <CoreSpotlight/CoreSpotlight.h>
+#import "FirstViewController.h"
+#import "SecondViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +20,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+     NSLog(@"%f",[UIDevice currentDevice].systemVersion.floatValue);
+    if ([UIDevice currentDevice].systemVersion.floatValue >= 9.0) {
+       
+        [self setSpotlight];
+    }
+    
+    
     return YES;
 }
 
@@ -42,4 +52,74 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (void)deleteSearchableItemsWithIdentifiers:(NSArray<NSString *> *)identifiers completionHandler:(void (^ __nullable)(NSError * __nullable error))completionHandler{
+
+}
+
+- (void)deleteSearchableItemsWithDomainIdentifiers:(NSArray<NSString *> *)domainIdentifiers completionHandler:(void (^ __nullable)(NSError * __nullable error))completionHandler{
+
+}
+
+- (void)deleteAllSearchableItemsWithCompletionHandler:(void (^ __nullable)(NSError * __nullable error))completionHandler{
+
+}
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray * _Nullable))restorationHandler{
+    
+    NSString *idetifier  = userActivity.userInfo[@"kCSSearchableItemActivityIdentifier"];
+    NSLog(@"%@",idetifier);
+    
+    UINavigationController *nav = (UINavigationController *)self.window.rootViewController;
+    UIStoryboard *stroy = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    
+    if ([idetifier isEqualToString:@"firstItem"]) {
+        
+        FirstViewController *fistVc = [stroy instantiateViewControllerWithIdentifier:@"firstView"];
+        [nav pushViewController:fistVc animated:YES];
+        
+    }else if ([idetifier isEqualToString:@"secondItem"]){
+        
+        SecondViewController *secondVc = [stroy instantiateViewControllerWithIdentifier:@"secondView"];
+        [nav pushViewController:secondVc animated:YES];
+    }
+    
+    return YES;
+}
+
+- (void)setSpotlight{
+    /*应用内搜索，想搜索到多少个界面就要创建多少个set ，每个set都要对应一个item*/
+    CSSearchableItemAttributeSet *firstSet = [[CSSearchableItemAttributeSet alloc] initWithItemContentType:@"firstSet"];
+    //标题
+    firstSet.title = @"测试firstView";
+    //详细描述
+    firstSet.contentDescription = @"测试firstView哈哈哈哈哈哈哈";
+    //关键字，
+    NSArray *firstSeachKey = [NSArray arrayWithObjects:@"first",@"测试",@"firstView", nil];
+    firstSet.contactKeywords = firstSeachKey;
+    
+    CSSearchableItemAttributeSet *secondSet = [[CSSearchableItemAttributeSet alloc] initWithItemContentType:@"secondSet"];
+    secondSet.title = @"测试SecondView";
+    secondSet.contentDescription = @"测试secondView哈哈哈哈哈哈哈哈";
+    NSArray *secondArrayKey = [NSArray arrayWithObjects:@"second",@"测试",@"secondeVIew", nil];
+    secondSet.contactKeywords = secondArrayKey;
+    
+    //UniqueIdentifier每个搜索都有一个唯一标示，当用户点击搜索到得某个内容的时候，系统会调用代理方法，会将这个唯一标示传给你，以便让你确定是点击了哪一，方便做页面跳转
+    //domainIdentifier搜索域标识，删除条目的时候调用的delegate会传过来这个值
+    CSSearchableItem *firstItem = [[CSSearchableItem alloc] initWithUniqueIdentifier:@"firstItem" domainIdentifier:@"first" attributeSet:firstSet];
+    
+    CSSearchableItem *secondItem = [[CSSearchableItem alloc] initWithUniqueIdentifier:@"secondItem" domainIdentifier:@"second" attributeSet:secondSet];
+    
+    NSArray *itemArray = [NSArray arrayWithObjects:firstItem,secondItem, nil];
+    
+    [[CSSearchableIndex defaultSearchableIndex] indexSearchableItems:itemArray completionHandler:^(NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"设置失败%@",error);
+
+        }else{
+        
+            NSLog(@"设置成功");
+        }
+    }];
+    
+}
 @end
---
+++ ios9SpotlightDemo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  ios9SpotlightDemo
+//
+//  Created by Rocky on 15/9/28.
+//  Copyright © 2015年 Rocky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
